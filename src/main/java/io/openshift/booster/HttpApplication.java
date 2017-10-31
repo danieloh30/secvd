eeb7927148ffec655f23549f1d3f6f98296da83d
Daniel Oh <doh@redhat.com>
2017-10-31 05:24:54 +0000

    updated greeting word with "Aloha"

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  protected static final String template = "Hello, %s!";
+  protected static final String template = "Aloha from SEC VD, %s!";
 
   @Override
   public void start(Future<Void> future) {
